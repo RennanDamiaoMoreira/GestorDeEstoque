2c7cb71ca973b6e14ff1441d9211e023b2791daa
Rennan <rennandamiao@gmail.com>
2021-05-03 14:27:31 -0300

    Update Teste.java

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -1,11 +1,38 @@
 package teste;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import DAO.BD;
+
 
 public class Teste {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-BD.getInstancia().getConexao();
+		Connection conexao = null;
+		PreparedStatement comando = null;
+	
+		
+
+		try {
+			conexao = BD.getInstancia().getConexao();
+			comando = conexao.prepareStatement("SELECT * FROM produto;");
+			ResultSet resultado = comando.executeQuery();
+
+			while (resultado.next()) {
+				System.out.println("id : "+resultado.getInt("id")+"  nome : "+resultado.getString("nome")+"  descricao : "+resultado.getString("descricao")+"\n");
+			}
+			
+		} finally {
+			
+		}
+
+		
 	}
 
 }

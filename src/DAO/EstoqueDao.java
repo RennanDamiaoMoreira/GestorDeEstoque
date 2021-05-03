696dceb4be54f0124a62146c4549328383eff5ec
Rennan <rennandamiao@gmail.com>
2021-05-03 14:37:29 -0300

    Update EstoqueDao.java

--- src/DAO/EstoqueDao.java
+++ src/DAO/EstoqueDao.java
@@ -3,10 +3,11 @@ package DAO;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Connection;
+import DAO.DAO;
 
 import model.Estoque;
 
-public class EstoqueDao {
+public class EstoqueDao extends DAO{
 	private static EstoqueDao instancia = new EstoqueDao();
 
 	public static EstoqueDao getInstacia() {
@@ -27,6 +28,19 @@ public class EstoqueDao {
 
 		} finally {
 			fecharConexao(conexao, comando);
+		}
+	}
+	public boolean alterar (Estoque estoque) {
+		Connection conexao = null;
+		PreparedStatement comando = null;
+		try {
+			conexao = BD.getInstancia().getConexao();
+			comando = conexao.prepareStatement("update estoque quantidade=? where produto=? and tamanho=?");
+			comando.setInt(1, estoque.getQuantidade());
+			comando.setInt(2, estoque.getProduto().getId());
+			comando.setString(3, estoque.getTamanho());
+			
+			
 		}
 	}
 
